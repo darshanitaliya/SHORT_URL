6cc058524de78381a5d4762713b0c852531cfbd1
Darshan Italiya <darshanitaliya7110@gmail.com>
2024-02-02 16:10:57 +0530

    Redirect URL route setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const urlRoute = require('./routes/url')
 const conectToMongoDB = require('./conntect')
+const URL = require('./model/url')
 
 const app = express()
 const PORT = 8000
@@ -12,5 +13,22 @@ conectToMongoDB('mongodb://127.0.0.1:27017/short-url').then(() =>
 app.use(express.json())
 
 app.use('/url', urlRoute)
+
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timeStamp: Date.now(),
+        },
+      },
+    }
+  )
+  res.redirect(entry.redirectUrl)
+})
 
 app.listen(PORT, () => console.log('Server Start'))
